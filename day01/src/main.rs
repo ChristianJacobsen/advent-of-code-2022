08ae2a77cea1a22fbab6fb788d41e00d8bcc2a9a
Christian A. Jacobsen <chrijaco@cisco.com>
2022-12-01 12:17:54 +0100

    Day 01

---
+++ day01/src/main.rs
@@ -0,0 +1,21 @@
+use std::{env::args, error::Error, fs::read_to_string};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file_path = args().nth(1).ok_or("missing input file")?;
+    let file_content = read_to_string(file_path)?;
+    let mut loads: Vec<_> = file_content
+        .split("\n\n")
+        .map(|load| {
+            load.lines().fold(0, |mut current, amount| {
+                current += amount.parse::<u32>().unwrap();
+                current
+            })
+        })
+        .collect();
+    loads.sort_by(|a, b| b.cmp(a));
+
+    println!("Part 1: {}", loads.first().unwrap());
+    println!("Part 2: {}", loads.iter().take(3).sum::<u32>());
+
+    Ok(())
+}
