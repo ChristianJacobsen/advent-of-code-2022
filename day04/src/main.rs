d1795a239608eb89e88504b36c5270dc626fc5bb
Christian A. Jacobsen <christian@jenkins.no>
2022-12-04 09:03:15 +0100

    Day 04

---
+++ day04/src/main.rs
@@ -0,0 +1,87 @@
+use std::{
+    cmp::{max, min},
+    error::Error,
+    str::FromStr,
+};
+
+use utils::read_input_file;
+
+struct Range {
+    start: u32,
+    end: u32,
+}
+
+impl Range {
+    fn len(&self) -> u32 {
+        self.end - self.start
+    }
+
+    fn overlapping_range(&self, r: &Self) -> Option<Self> {
+        if self.start > r.end || self.end < r.start {
+            return None;
+        }
+
+        Some(Self {
+            start: max(self.start, r.start),
+            end: min(self.end, r.end),
+        })
+    }
+
+    fn overlaps(&self, r: &Self) -> bool {
+        return self.start <= r.end && self.end >= r.start;
+    }
+
+    fn fully_overlaps(&self, r: &Self) -> bool {
+        if let Some(overlap) = self.overlapping_range(r) {
+            let overlap_length = overlap.len();
+            return self.len() == overlap_length || r.len() == overlap_length;
+        }
+
+        false
+    }
+}
+
+impl FromStr for Range {
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (start, end) = s.split_once('-').ok_or("could not extract start/end")?;
+        Ok(Self {
+            start: start.parse()?,
+            end: end.parse()?,
+        })
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file_content = read_input_file()?;
+
+    let mut sum_part_1 = 0;
+    for pair in file_content.lines() {
+        let (elf_1, elf_2) = pair.split_once(',').ok_or("could not split pair")?;
+
+        let elf_1 = elf_1.parse::<Range>()?;
+        let elf_2 = elf_2.parse::<Range>()?;
+
+        if elf_1.fully_overlaps(&elf_2) {
+            sum_part_1 += 1;
+        }
+    }
+
+    let mut sum_part_2 = 0;
+    for pair in file_content.lines() {
+        let (elf_1, elf_2) = pair.split_once(',').ok_or("could not split pair")?;
+
+        let elf_1 = elf_1.parse::<Range>()?;
+        let elf_2 = elf_2.parse::<Range>()?;
+
+        if elf_1.overlaps(&elf_2) {
+            sum_part_2 += 1;
+        }
+    }
+
+    println!("Part 1: {}", sum_part_1);
+    println!("Part 2: {}", sum_part_2);
+
+    Ok(())
+}
