9221ad66520f9d5e882c2355fc87bb3c856c6579
Christian A. Jacobsen <christian@jenkins.no>
2022-12-16 21:44:30 +0100

    Day 16 done

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,76 +1,66 @@
-use std::{cmp::max, collections::HashMap, error::Error};
+use std::{cmp::max, collections::BTreeMap, error::Error};
 
+use cached::proc_macro::cached;
 use itertools::Itertools;
 use utils::read_input_file;
 
-struct Storage<'a> {
-    flow_rates: HashMap<&'a str, usize>,
-    adjacent_valves: HashMap<&'a str, Vec<&'a str>>,
-    lookup: HashMap<(&'a str, Vec<&'a str>, i32), usize>,
-}
-
-impl<'a> Storage<'a> {
-    fn new(
-        flow_rates: HashMap<&'a str, usize>,
-        adjacent_valves: HashMap<&'a str, Vec<&'a str>>,
-    ) -> Self {
-        Self {
-            flow_rates,
-            adjacent_valves,
-            lookup: HashMap::new(),
-        }
-    }
-}
-
-fn max_flow<'a>(
-    storage: &'a mut Storage<'a>,
-    current_valve: &'a str,
-    open_valves: Vec<&'a str>,
+#[cached(
+    key = "String",
+    convert = r#"{ format!("{}:{:?}:{}:{}", current_valve, open_valves, minutes_left, part_2) }"#
+)]
+fn max_flow(
+    flow_rates: &BTreeMap<&str, usize>,
+    adjacent_valves: &BTreeMap<&str, Vec<&str>>,
+    current_valve: &str,
+    open_valves: Vec<&str>,
     minutes_left: i32,
+    part_2: bool,
 ) -> usize {
-    if let Some(&flow_rate) =
-        storage
-            .lookup
-            .get(&(current_valve, open_valves.clone(), minutes_left))
-    {
-        return flow_rate;
-    }
-
     if minutes_left <= 0 {
-        return 0;
+        if part_2 {
+            return max_flow(flow_rates, adjacent_valves, "AA", open_valves, 26, false);
+        } else {
+            return 0;
+        }
     }
 
     let mut best_flow = 0;
 
-    if !open_valves.contains(&current_valve) {
-        let flow = (minutes_left - 1) as usize * storage.flow_rates[current_valve];
+    if open_valves.binary_search(&current_valve).is_err() {
+        let total_flow = (minutes_left - 1) as usize * flow_rates[current_valve];
 
-        let mut open_valves = open_valves.clone();
-        open_valves.push(current_valve);
-        open_valves.sort();
+        let mut currently_open_valves = open_valves.clone();
+        currently_open_valves.push(current_valve);
+        currently_open_valves.sort();
 
-        let adjacent_valves = storage.adjacent_valves.clone();
-        for valve in &adjacent_valves[current_valve] {
-            if flow != 0 {
-                best_flow = max(
-                    best_flow,
-                    flow + max_flow(storage, valve, open_valves.clone(), minutes_left - 2),
-                );
-
-                storage
-                    .lookup
-                    .insert((valve, open_valves.clone(), minutes_left - 2), best_flow);
-            }
-
+        if total_flow != 0 {
             best_flow = max(
                 best_flow,
-                max_flow(storage, valve, open_valves.clone(), minutes_left - 1),
+                total_flow
+                    + max_flow(
+                        flow_rates,
+                        adjacent_valves,
+                        current_valve,
+                        currently_open_valves,
+                        minutes_left - 1,
+                        part_2,
+                    ),
             );
-
-            storage
-                .lookup
-                .insert((valve, open_valves.clone(), minutes_left - 1), best_flow);
         }
+    }
+
+    for &valve in &adjacent_valves[current_valve] {
+        best_flow = max(
+            best_flow,
+            max_flow(
+                flow_rates,
+                adjacent_valves,
+                valve,
+                open_valves.clone(),
+                minutes_left - 1,
+                part_2,
+            ),
+        );
     }
 
     best_flow
@@ -79,8 +69,8 @@ fn max_flow<'a>(
 fn main() -> Result<(), Box<dyn Error>> {
     let file_content = read_input_file()?;
 
-    let mut flow_rates = HashMap::new();
-    let mut adjacent_valves = HashMap::new();
+    let mut flow_rates = BTreeMap::new();
+    let mut adjacent_valves = BTreeMap::new();
     for line in file_content.lines() {
         let (valve_part, adjacent_valves_part) =
             line.split_once(';').ok_or("could not split line")?;
@@ -96,11 +86,11 @@ fn main() -> Result<(), Box<dyn Error>> {
         adjacent_valves.insert(valve, valves);
     }
 
-    let mut storage = Storage::new(flow_rates, adjacent_valves);
-
-    let part_1 = max_flow(&mut storage, "AA", Vec::new(), 30);
+    let part_1 = max_flow(&flow_rates, &adjacent_valves, "AA", Vec::new(), 30, false);
+    let part_2 = max_flow(&flow_rates, &adjacent_valves, "AA", Vec::new(), 26, true);
 
     println!("Part 1: {}", part_1);
+    println!("Part 2: {}", part_2);
 
     Ok(())
 }
